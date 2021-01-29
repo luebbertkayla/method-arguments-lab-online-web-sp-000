4f9d5518dcc553fc469bcdf31c779569667f98f5
Kayla Luebbert <luebbertkayla@gmail.com>
2021-01-29 03:11:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,6 @@ name = "Josh"
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
